db2a92732d5c1bd053c8ae7f3f7958c2c33dfb38
Gerson Aarón Matta Aguilar <gmatta-2021223@kinal.edu.gt>
2023-05-07 14:39:02 -0600

    Se creó el archivo Home, el componente Sidebar y la página de Profile Settings (Update User) y el CSS de todos estos.

---
+++ aec-react/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import './styleDashboard.css'
+import {Sidebar} from '../../components/Sidebar'
+
+export const Dashboard = () => {
+    return (
+
+        <>
+            <div className="container-fluid">
+                
+                <div className="row flex-nowrap">
+
+                    <div className="bgDash col-auto col-md-3 col-xl-2 px-sm-2 px-0 sticky">
+                        
+                        <Sidebar/>
+
+                    </div>
+
+                    <div className="col py-3">
+
+                       {/* CONTENIDO */}
+                        <Outlet/>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </>
+
+    )
+
+}
