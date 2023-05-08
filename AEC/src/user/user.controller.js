ae2e40feb02b46fef818839d1998743c1f93f361
jcalderon-2018148 <jcalderon-2018148@kinal.edu.gt>
2023-05-08 18:24:42 -0500

    Se implementaron métodos y rutas de usuario.

--- AEC/src/user/user.controller.js
+++ AEC/src/user/user.controller.js
@@ -10,6 +10,8 @@ exports.test = (req, res) => {
     res.send({ message: 'Test users' })
 }
 
+//PARA METER OBJETOS A LOCALSTORAGE ---- AL METER JSON.stringify() y al sacar JSON.parse()
+
 /* ----- DEFAULT MASTER ----- */
 exports.defaultMaster = async() => {
     try {
@@ -89,7 +91,7 @@ exports.register = async(req, res) => {
         if(msg) return res.status(400).send({ message: msg })
 
         data.role = ROLES.client
-        data.password = encrypt(data.password)
+        data.password = await encrypt(data.password)
         
         let user = new User(data)
         await user.save()
@@ -155,5 +157,131 @@ exports.update = async(req, res) => {
     } catch (err) {
         console.error(err)
         return res.status(500).send({ message: 'Error updating user :(', error: err })
+    }
+}
+
+/* -----DELETE ----- */
+exports.del = async(req, res) => {
+    try {
+        let id = req.user.sub
+
+        let user = await User.findOne({ _id: id })
+        if (!user) return res.status(404).send({ message: 'User not found to be deleted :(' })
+        if (user.username === 'admin') return res.status(403).send({ message: 'User MASTER default cannot be deleted' })
+        if (user.role === ROLES.admin || user.role === ROLES.master) return res.status(403).send({ message: `User with role "${user.role}" cannot be deleted` })
+
+        let delUser = await User.findOneAndDelete({_id: id})
+
+        if(!delUser) return res.status(404).send({ message: 'Account could not be deleted :(' })
+        return res.send({ message: 'Account deleted successfully!', error: err })
+
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({ message: 'Error deleting acount :(', error: err })
+    }
+}
+
+/* -----UPDATE PASSWORD ----- */
+exports.updatePassword = async(req, res) => {
+    try {
+        let data = req.body
+        let form = {
+            password: data.password,
+            newPassword: data.newPass
+        }
+        let msg = validateData(form)
+        if(msg) return res.status(400).send({ message: msg })
+
+        let user = await User.findOne({ _id: id })
+
+        if (user && await check(data.password, user.password)) {
+            await User.findOneAndUpdate(
+                { _id: req.user.sub },
+                { password: await encrypt(data.newPass) }
+            )
+
+            return res.send({ message: 'Password updated!' })
+        }
+
+        return res.status(401).send({ message: 'Password does not coincide!' })
+
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({ message: 'Error while updating password', error: err })
+    }
+}
+
+/* -----SAVE ACCOUNT ----- */
+exports.save = async(req, res) => {
+    try {
+        let data = req.body
+        let params = {
+            name: data.name,
+            surname: data.surname,
+            phone: data.phone,
+            email: data.email,
+            password: data.password,
+            username: data.username,
+            role: data.role
+        }
+
+        let msg = validateData(params)
+        if (msg) return res.status(400).send({ message: msg })
+        
+        data.password = await encrypt(data.password)
+        data.role = data.role.toUpperCase()
+
+        let user = new User()
+        await user.save()
+
+        return res.send({ message: 'Account created successfully!', user: user })
+
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({ message: 'Error saving new account :(', error: err })
+    }
+}
+
+/* -----UPDATE ACCOUNT ----- */
+exports.update = async(req, res) => {
+    try {
+        let id = req.params.id
+        let data = req.body
+        let user = await User.findOne({ _id: id })
+
+        if (!user) return res.status(404).send({ message: 'User not found!' })
+        if (data.password) return res.status(401).send({ message: 'Cannot update password!' })
+        if (user.role === ROLES.master) return res.status(401).send({ message: 'Cannot update "MASTER"' })
+
+        let upUser = await User.findOneAndUpdate(
+            { _id: id },
+            data,
+            { new: true }
+        )
+
+        return res.send({ message: 'Account updated successfully!', user: upUser })
+        
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({ message: 'Error while updating account :(', error: err })
+    }
+}
+
+/* -----DELETE ACCOUNT ----- */
+exports.delUser = async(req, res) => {
+    try {
+        let id = req.params.id
+
+        let user = await User.findOne({ _id: id })
+        if (!user) return res.status(404).send({ message: 'User not found' })
+        if (user.role === ROLES.master) return res.status(401).send({ message: 'Cannot delete user "MASTER"' })
+
+        let delUser = await User.findOneAndDelete({ _id: id })
+
+        return res.send({ message: 'Account deleted successfully!', user: delUser })
+
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({ message: 'Error while deleting account :(', error: err })
     }
 }
