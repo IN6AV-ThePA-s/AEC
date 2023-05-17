f1fa8f8055f86d4557b1ff7493728f6cbae780f2
Gerson Aarón Matta Aguilar <gmatta-2021223@kinal.edu.gt>
2023-05-17 15:03:21 -0600

    Se arreglaron rutas de Events.

--- AEC/src/event/event.routes.js
+++ AEC/src/event/event.routes.js
@@ -5,11 +5,11 @@ const eventController = require('./event.controller')
 const { ensureAdvance, isAdmin, isMaster } = require('../services/authenticated')
 
 api.get('/test', eventController.test)
-api.post('/add',[ensureAdvance,isAdmin, isMaster], eventController.addEvent)
+api.post('/add',[ensureAdvance,isAdmin], eventController.addEvent)
 api.get('/get', ensureAdvance, eventController.getEvents)
 api.get('/getByHotel/:id', ensureAdvance, eventController.getEventsByHotel)
-api.get('/getOne/:id',[ensureAdvance,isAdmin, isMaster], eventController.getEvent)
-api.delete('/delete/:id',[ensureAdvance,isAdmin, isMaster],  eventController.deleteEvent)
-api.put('/update/:id',[ensureAdvance,isAdmin, isMaster],  eventController.updateEvent)
+api.get('/getOne/:id',[ensureAdvance,isAdmin], eventController.getEvent)
+api.delete('/delete/:id',[ensureAdvance,isAdmin],  eventController.deleteEvent)
+api.put('/update/:id',[ensureAdvance,isAdmin],  eventController.updateEvent)
 
 module.exports = api
