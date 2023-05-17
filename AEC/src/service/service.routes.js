7da61edcf899110a78879568086101493e82b669
jcalderon-2018148 <jcalderon-2018148@kinal.edu.gt>
2023-05-16 22:21:17 -0600

    Se consumio API para coleccion room.

--- AEC/src/service/service.routes.js
+++ AEC/src/service/service.routes.js
@@ -8,8 +8,8 @@ api.get('/test', [ensureAdvance, isAdmin], serviceController.test);
 api.get('/get', [ensureAdvance], serviceController.getServices)
 api.get('/get/:id', [ensureAdvance], serviceController.getService)
 api.get('/get-hotel-service/:hotel', [ensureAdvance], serviceController.getServicesByHotel)
-api.post('/add', [ensureAdvance, isMaster], serviceController.addService)
-api.put('/update/:id', [ensureAdvance, isMaster], serviceController.updateService)
-api.delete('/delete/:id', [ensureAdvance, isMaster], serviceController.deleteService);
+api.post('/add', [ensureAdvance, isAdmin], serviceController.addService)
+api.put('/update/:id', [ensureAdvance, isAdmin], serviceController.updateService)
+api.delete('/delete/:id', [ensureAdvance, isAdmin], serviceController.deleteService);
 
 module.exports = api;
